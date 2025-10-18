bf4997e9c247cd69b823fa7f7c392bb644ba95b4
jagritih3-spec <jagritih3@gmail.com>
2025-10-18 12:13:10 +0530

    testing3

---
+++ Jagriti/1to10.py
@@ -0,0 +1,16 @@
+#p1
+print("hello world")
+#p2
+num = 10
+price = 99.99
+name = "Jiya"
+is_student = True
+fruits =["banana", "mango", "grapes"]
+coordinates = (1,2)
+person ={"name": "Jiya", "age": 20}
+unique_numbers = {1,2,3,4,5}
+#p3
+a= 1
+b= 2
+a,b = b,a
+print(a,b)
