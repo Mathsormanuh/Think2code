167feb410fc12dad9ecf7268329563b1a4233eb8
chakmaritesh220 <chakmaritesh220@gmail.com>
2025-10-18 12:11:01 +0530

    Ritesh

---
+++ Ritesh/First.py
@@ -0,0 +1,18 @@
+# #1
+# print("My name is Ritesh Chakma ")
+
+# #2
+# print(10-3)
+# print("The answer is = ", 10-3)
+
+# #3
+# print(5+7)
+# print('The addition of 5 and 7 is =',5+7)
+
+#4
+for i in range (1,10):
+    print i
+
+
+
+
