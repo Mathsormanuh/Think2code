e58ff81e5d567d65364f85a911ea591e4b0bd0f9
Sumitra <sumitragogoi2004@gmail.com>
2025-10-23 11:09:56 +0530

    1-20 sums done

---
+++ Sumitra/level 1/2.py
@@ -0,0 +1,16 @@
+a = 25
+b = 10.5
+name = "Sumitra"
+is_student = True
+numbers = [1, 2, 3, 4, 5]
+colors = ("red", "green", "blue")
+unique_values = {10, 20, 30}
+person = {"name": "Rahul", "age": 28, "city": "Guwahati"}
+print("Integer:", a)
+print("Float:", b)
+print("String:", name)
+print("Boolean:", is_student)
+print("List:", numbers)
+print("Tuple:", colors)
+print("Set:", unique_values)
+print("Dictionary:", person)
